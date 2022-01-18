b4ea34e20d404e5a9238d66e6f89da1778062b0e
Neha Aggarwal <neha.aggarwal@version1.com>
2022-01-18 09:56:40 +0000

    WIP_ Java objects

---
+++ src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/BailCaseFieldDefinition.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.bailcaseapi.domain.entities;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+public enum BailCaseFieldDefinition {
+    //Example
+    APPELLANT_GIVEN_NAMES(
+        "appellantGivenNames", new TypeReference<String>(){});
+
+
+    private final String value;
+    private final TypeReference typeReference;
+
+    BailCaseFieldDefinition(String value, TypeReference typeReference) {
+        this.value = value;
+        this.typeReference = typeReference;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public TypeReference getTypeReference() {
+        return typeReference;
+    }
+}
