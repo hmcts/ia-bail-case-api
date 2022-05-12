656c9c7e1a41b30c0334037c14d93adcfed28ea2
rajeshthuraiyur <rajesh.thuraiyur@gmail.com>
2022-05-12 16:56:28 +0100

    Updates to fix deployment issues

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,7 +4,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.25"
+      version = "~> 2.78.0"
+    }
+    random = {
+      source = "hashicorp/random"
     }
     azuread = {
       source  = "hashicorp/azuread"
