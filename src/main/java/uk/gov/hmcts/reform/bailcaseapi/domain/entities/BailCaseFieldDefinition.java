9f38c37a548bfc03f1c2199556b9171d7f5849ba
Jamie <jamiemurphy@version1.com>
2022-05-31 15:49:51 +0100

    Change due date

--- src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/BailCaseFieldDefinition.java
+++ src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/BailCaseFieldDefinition.java
@@ -308,7 +308,7 @@ public enum BailCaseFieldDefinition {
     DIRECTIONS(
         "directions", new TypeReference<List<IdValue<Direction>>>(){}),
     BAIL_DIRECTION_LIST(
-        "bailDirectionList", new TypeReference<String>(){}),
+        "bailDirectionList", new TypeReference<DynamicList>(){}),
     BAIL_DIRECTION_EDIT_EXPLANATION(
         "bailDirectionEditExplanation", new TypeReference<String>(){}),
     BAIL_DIRECTION_EDIT_PARTIES(
@@ -317,6 +317,8 @@ public enum BailCaseFieldDefinition {
         "bailDirectionEditDateSent", new TypeReference<String>(){}),
     BAIL_DIRECTION_EDIT_DATE_DUE(
         "bailDirectionEditDateDue", new TypeReference<String>(){}),
+    EDITABLE_DIRECTIONS(
+        "editableDirections", new TypeReference<List<IdValue<EditableDirection>>>(){}),
     REASON_FOR_REFUSAL_DETAILS(
         "reasonForRefusalDetails", new TypeReference<String>(){}),
     TRIBUNAL_REFUSAL_REASON(
--- src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/Direction.java
+++ src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/Direction.java
@@ -2,6 +2,9 @@ package uk.gov.hmcts.reform.bailcaseapi.domain.entities;
 
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.field.IdValue;
+
+import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
@@ -13,6 +16,7 @@ public class Direction {
     private String sendDirectionList;
     private String dateOfCompliance;
     private String dateSent;
+    private List<IdValue<PreviousDates>> previousDates;
 
     private Direction() {
     }
@@ -31,6 +35,22 @@ public class Direction {
 
     }
 
+    public Direction(
+        String sendDirectionDescription,
+        String sendDirectionList,
+        String dateOfCompliance,
+        String dateSent,
+        List<IdValue<PreviousDates>> previousDates
+
+    ) {
+        this.sendDirectionDescription = requireNonNull(sendDirectionDescription);
+        this.sendDirectionList = requireNonNull(sendDirectionList);
+        this.dateOfCompliance = requireNonNull(dateOfCompliance);
+        this.dateSent = requireNonNull(dateSent);
+        this.previousDates = requireNonNull(previousDates);
+
+    }
+
 
     public String getSendDirectionDescription() {
         return requireNonNull(sendDirectionDescription);
@@ -46,6 +66,10 @@ public class Direction {
 
     public String getDateSent() {
         return requireNonNull(dateSent);
+    }
+
+    public List<IdValue<PreviousDates>> getPreviousDates() {
+        return requireNonNull(previousDates);
     }
 
 
