7020451578024ed3cb0df25deb0b2be9c405a26c
Ryan-Nesbitt <95298094+Ryan-Nesbitt@users.noreply.github.com>
2022-06-10 11:36:24 +0100

    Handler to remove fields from Case Data upon editing

--- src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/BailCase.java
+++ src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/BailCase.java
@@ -39,4 +39,8 @@ public class BailCase extends HashMap<String, Object> implements CaseData {
     public void clear(BailCaseFieldDefinition extractor) {
         this.put(extractor.value(), null);
     }
+
+    public void remove(BailCaseFieldDefinition field) {
+        this.remove(field.value());
+    }
 }
---
+++ src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/handlers/presubmit/EditAppealHandler.java
@@ -0,0 +1,250 @@
+package uk.gov.hmcts.reform.bailcaseapi.domain.handlers.presubmit;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.*;
+import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.field.YesOrNo.NO;
+
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCase;
+import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.Event;
+import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.field.YesOrNo;
+import uk.gov.hmcts.reform.bailcaseapi.domain.handlers.PreSubmitCallbackHandler;
+
+@Slf4j
+@Component
+public class EditAppealHandler implements PreSubmitCallbackHandler<BailCase> {
+
+    public boolean canHandle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<BailCase> callback
+    ) {
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+               && (callback.getEvent() == Event.EDIT_BAIL_APPLICATION);
+    }
+
+    public PreSubmitCallbackResponse<BailCase> handle(
+        PreSubmitCallbackStage callbackStage,
+        Callback<BailCase> callback
+    ) {
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        BailCase bailCase =
+            callback
+                .getCaseDetails()
+                .getCaseData();
+
+        Optional<YesOrNo> optionalFinancialSupporter1 = bailCase.read(HAS_FINANCIAL_COND_SUPPORTER, YesOrNo.class);
+        Optional<YesOrNo> optionalFinancialSupporter2 = bailCase.read(HAS_FINANCIAL_COND_SUPPORTER_2, YesOrNo.class);
+        Optional<YesOrNo> optionalFinancialSupporter3 = bailCase.read(HAS_FINANCIAL_COND_SUPPORTER_3, YesOrNo.class);
+        Optional<YesOrNo> optionalFinancialSupporter4 = bailCase.read(HAS_FINANCIAL_COND_SUPPORTER_4, YesOrNo.class);
+        Optional<YesOrNo> optionalInterpreterLanguage = bailCase.read(INTERPRETER_YESNO, YesOrNo.class);
+        Optional<String> optionalNationality = bailCase.read(APPLICANT_NATIONALITY, String.class);
+        Optional<YesOrNo> optionalBailEvidenceDocs = bailCase.read(GROUNDS_FOR_BAIL_PROVIDE_EVIDENCE_OPTION, YesOrNo.class);
+        Optional<YesOrNo> optionalLegalRepDetails = bailCase.read(HAS_LEGAL_REP, YesOrNo.class);
+        Optional<String> optionalDetentionLocation = bailCase.read(APPLICANT_DETENTION_LOCATION, String.class);
+        Optional<String> optionalGender = bailCase.read(APPLICANT_GENDER, String.class);
+        Optional<YesOrNo> optionalApplicantMobileNumber = bailCase.read(APPLICANT_HAS_MOBILE, YesOrNo.class);
+
+        YesOrNo hasFinancialConditionSupporter1 = bailCase.read(HAS_FINANCIAL_COND_SUPPORTER, YesOrNo.class).orElse(NO);
+        YesOrNo hasFinancialConditionSupporter2 = bailCase.read(HAS_FINANCIAL_COND_SUPPORTER_2, YesOrNo.class).orElse(NO);
+        YesOrNo hasFinancialConditionSupporter3 = bailCase.read(HAS_FINANCIAL_COND_SUPPORTER_3, YesOrNo.class).orElse(NO);
+        YesOrNo hasFinancialConditionSupporter4 = bailCase.read(HAS_FINANCIAL_COND_SUPPORTER_4, YesOrNo.class).orElse(NO);
+
+        if (optionalApplicantMobileNumber.isPresent()) {
+            YesOrNo applicantMobilePhoneValue = optionalApplicantMobileNumber.get();
+
+            if (applicantMobilePhoneValue.equals(NO)){
+                bailCase.remove(APPLICANT_MOBILE_NUMBER);
+            }
+        }
+
+        if (optionalGender.isPresent()) {
+            String genderValue = optionalGender.get();
+
+            if (genderValue.equals("female") || genderValue.equals("male")){
+                bailCase.remove(APPLICANT_GENDER_OTHER);
+            }
+        }
+
+        if (optionalDetentionLocation.isPresent()) {
+            String detentionFacilityValue = optionalDetentionLocation.get();
+
+            if (detentionFacilityValue.equals("immigrationRemovalCentre")){
+                bailCase.remove(PRISON_NAME);
+                bailCase.remove(APPLICANT_PRISON_DETAILS);
+            } else if (detentionFacilityValue.equals("prison")) {
+                bailCase.remove(IRC_NAME);
+            }
+        }
+
+        if (optionalLegalRepDetails.isPresent()) {
+            YesOrNo legalRepDetails = optionalLegalRepDetails.get();
+
+            if (legalRepDetails.equals(NO)){
+                clearLegalRepDetails(bailCase);
+            }
+        }
+
+        if (optionalBailEvidenceDocs.isPresent()) {
+            YesOrNo bailEvidenceDocs = optionalBailEvidenceDocs.get();
+
+            if (bailEvidenceDocs.equals(NO)){
+                bailCase.remove(BAIL_EVIDENCE);
+            }
+        }
+
+        if (optionalInterpreterLanguage.isPresent()) {
+            YesOrNo interpreterLanguages = optionalInterpreterLanguage.get();
+
+            if (interpreterLanguages.equals(NO)){
+                bailCase.remove(INTERPRETER_LANGUAGES);
+            }
+        }
+
+        if (optionalNationality.isPresent()) {
+            String nationalityValue = optionalNationality.get();
+
+            if (nationalityValue.equals("STATELESS")){
+                bailCase.remove(APPLICANT_NATIONALITIES);
+            }
+        }
+
+        if (optionalFinancialSupporter1.isPresent()) {
+
+            //Clear all the supporter 1-4 fields
+            if (hasFinancialConditionSupporter1.equals(NO)) {
+                log.info("Clearing Financial Supporter details from bail application case data");
+                clearFinancialSupporter1Details(bailCase);
+                clearFinancialSupporter2Details(bailCase);
+                clearFinancialSupporter3Details(bailCase);
+                clearFinancialSupporter4Details(bailCase);
+            }
+        }
+
+        if (optionalFinancialSupporter2.isPresent()) {
+
+            //Clear all the supporter 2-4 fields
+            if (hasFinancialConditionSupporter2.equals(NO)) {
+                log.info("Clearing Financial Supporter details from bail application case data");
+                clearFinancialSupporter2Details(bailCase);
+                clearFinancialSupporter3Details(bailCase);
+                clearFinancialSupporter4Details(bailCase);
+            }
+        }
+
+        if (optionalFinancialSupporter3.isPresent()) {
+
+            //Clear all the supporter 3-4 fields
+            if (hasFinancialConditionSupporter3.equals(NO)) {
+                log.info("Clearing Financial Supporter details from bail application case data");
+                clearFinancialSupporter3Details(bailCase);
+                clearFinancialSupporter4Details(bailCase);
+            }
+        }
+
+        if (optionalFinancialSupporter4.isPresent()) {
+
+            //Clear all the supporter 4 fields
+            if (hasFinancialConditionSupporter4.equals(NO)) {
+                log.info("Clearing Financial Supporter details from bail application case data");
+                clearFinancialSupporter4Details(bailCase);
+            }
+        }
+
+        return new PreSubmitCallbackResponse<>(bailCase);
+    }
+
+    private void clearFinancialSupporter1Details(BailCase bailCase) {
+        bailCase.remove(SUPPORTER_GIVEN_NAMES);
+        bailCase.remove(SUPPORTER_FAMILY_NAMES);
+        bailCase.remove(SUPPORTER_ADDRESS_DETAILS);
+        bailCase.remove(SUPPORTER_CONTACT_DETAILS);
+        bailCase.remove(SUPPORTER_TELEPHONE_NUMBER);
+        bailCase.remove(SUPPORTER_MOBILE_NUMBER);
+        bailCase.remove(SUPPORTER_EMAIL_ADDRESS);
+        bailCase.remove(SUPPORTER_DOB);
+        bailCase.remove(SUPPORTER_RELATION);
+        bailCase.remove(SUPPORTER_OCCUPATION);
+        bailCase.remove(SUPPORTER_IMMIGRATION);
+        bailCase.remove(SUPPORTER_NATIONALITY);
+        bailCase.remove(SUPPORTER_HAS_PASSPORT);
+        bailCase.remove(SUPPORTER_PASSPORT);
+        bailCase.remove(FINANCIAL_AMOUNT_SUPPORTER_UNDERTAKES);
+    }
+
+    private void clearFinancialSupporter2Details(BailCase bailCase) {
+        bailCase.remove(SUPPORTER_2_GIVEN_NAMES);
+        bailCase.remove(SUPPORTER_2_FAMILY_NAMES);
+        bailCase.remove(SUPPORTER_2_ADDRESS_DETAILS);
+        bailCase.remove(SUPPORTER_2_CONTACT_DETAILS);
+        bailCase.remove(SUPPORTER_2_TELEPHONE_NUMBER);
+        bailCase.remove(SUPPORTER_2_MOBILE_NUMBER);
+        bailCase.remove(SUPPORTER_2_EMAIL_ADDRESS);
+        bailCase.remove(SUPPORTER_2_DOB);
+        bailCase.remove(SUPPORTER_2_RELATION);
+        bailCase.remove(SUPPORTER_2_OCCUPATION);
+        bailCase.remove(SUPPORTER_2_IMMIGRATION);
+        bailCase.remove(SUPPORTER_2_NATIONALITY);
+        bailCase.remove(SUPPORTER_2_HAS_PASSPORT);
+        bailCase.remove(SUPPORTER_2_PASSPORT);
+        bailCase.remove(FINANCIAL_AMOUNT_SUPPORTER_2_UNDERTAKES);
+        bailCase.remove(HAS_FINANCIAL_COND_SUPPORTER_2);
+    }
+
+    private void clearFinancialSupporter3Details(BailCase bailCase) {
+        bailCase.remove(SUPPORTER_3_GIVEN_NAMES);
+        bailCase.remove(SUPPORTER_3_FAMILY_NAMES);
+        bailCase.remove(SUPPORTER_3_ADDRESS_DETAILS);
+        bailCase.remove(SUPPORTER_3_CONTACT_DETAILS);
+        bailCase.remove(SUPPORTER_3_TELEPHONE_NUMBER);
+        bailCase.remove(SUPPORTER_3_MOBILE_NUMBER);
+        bailCase.remove(SUPPORTER_3_EMAIL_ADDRESS);
+        bailCase.remove(SUPPORTER_3_DOB);
+        bailCase.remove(SUPPORTER_3_RELATION);
+        bailCase.remove(SUPPORTER_3_OCCUPATION);
+        bailCase.remove(SUPPORTER_3_IMMIGRATION);
+        bailCase.remove(SUPPORTER_3_NATIONALITY);
+        bailCase.remove(SUPPORTER_3_HAS_PASSPORT);
+        bailCase.remove(SUPPORTER_3_PASSPORT);
+        bailCase.remove(FINANCIAL_AMOUNT_SUPPORTER_3_UNDERTAKES);
+        bailCase.remove(HAS_FINANCIAL_COND_SUPPORTER_3);
+    }
+
+    private void clearFinancialSupporter4Details(BailCase bailCase) {
+        bailCase.remove(SUPPORTER_4_GIVEN_NAMES);
+        bailCase.remove(SUPPORTER_4_FAMILY_NAMES);
+        bailCase.remove(SUPPORTER_4_ADDRESS_DETAILS);
+        bailCase.remove(SUPPORTER_4_CONTACT_DETAILS);
+        bailCase.remove(SUPPORTER_4_TELEPHONE_NUMBER);
+        bailCase.remove(SUPPORTER_4_MOBILE_NUMBER);
+        bailCase.remove(SUPPORTER_4_EMAIL_ADDRESS);
+        bailCase.remove(SUPPORTER_4_DOB);
+        bailCase.remove(SUPPORTER_4_RELATION);
+        bailCase.remove(SUPPORTER_4_OCCUPATION);
+        bailCase.remove(SUPPORTER_4_IMMIGRATION);
+        bailCase.remove(SUPPORTER_4_NATIONALITY);
+        bailCase.remove(SUPPORTER_4_HAS_PASSPORT);
+        bailCase.remove(SUPPORTER_4_PASSPORT);
+        bailCase.remove(FINANCIAL_AMOUNT_SUPPORTER_4_UNDERTAKES);
+        bailCase.remove(HAS_FINANCIAL_COND_SUPPORTER_4);
+    }
+
+    private void clearLegalRepDetails(BailCase bailCase) {
+        bailCase.remove(LEGAL_REP_COMPANY);
+        bailCase.remove(LEGAL_REP_EMAIL_ADDRESS);
+        bailCase.remove(LEGAL_REP_NAME);
+        bailCase.remove(LEGAL_REP_PHONE);
+        bailCase.remove(LEGAL_REP_REFERENCE);
+    }
+
+}
