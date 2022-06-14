12c0914f3d958536bb0f7d70ef4c062ba1e10edb
Dbarnes-v1 <daniel.barnes@hmcts.net>
2022-06-14 12:47:09 +0100

    RIA-5580 - unit test fix

--- src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/handlers/presubmit/ApplicationUserRoleAppenderTest.java
+++ src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/handlers/presubmit/ApplicationUserRoleAppenderTest.java
@@ -193,10 +193,10 @@ class ApplicationUserRoleAppenderTest {
                 boolean canHandle = applicationUserRoleAppender.canHandle(callbackStage, callback);
                 if (callbackStage == ABOUT_TO_START
                         && (callback.getEvent() == Event.START_APPLICATION
-                            || callback.getEvent() == Event.MAKE_NEW_APPLICATION)
-                    || (callbackStage == ABOUT_TO_SUBMIT
-                        && (callback.getEvent() == Event.MAKE_NEW_APPLICATION
-                            || callback.getEvent() == Event.EDIT_BAIL_APPLICATION_AFTER_SUBMIT))) {
+                            || callback.getEvent() == Event.MAKE_NEW_APPLICATION
+                            || callback.getEvent() == Event.EDIT_BAIL_APPLICATION_AFTER_SUBMIT)
+                    || callbackStage == ABOUT_TO_SUBMIT
+                        && callback.getEvent() == Event.MAKE_NEW_APPLICATION) {
                     assertTrue(canHandle);
                 } else {
                     assertFalse(canHandle);
