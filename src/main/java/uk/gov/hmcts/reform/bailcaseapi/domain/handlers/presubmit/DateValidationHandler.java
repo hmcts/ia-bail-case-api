ae444572ee89edfbbe8e9d274e1149deda12f9a4
Marcellofabbri <marcello.fabbri@hmcts.net>
2022-06-23 11:17:35 +0100

    RIA-5603 Add DateValidationHandlerTest

--- src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/handlers/presubmit/DateValidationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/handlers/presubmit/DateValidationHandler.java
@@ -3,19 +3,10 @@ package uk.gov.hmcts.reform.bailcaseapi.domain.handlers.presubmit;
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.APPLICANT_ARRIVAL_IN_UK;
 import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.BAIL_HEARING_DATE;
-import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.DECISION_DETAILS_DATE;
-import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.DECISION_GRANTED_OR_REFUSED;
-import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.RECORD_DECISION_TYPE;
-import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.RECORD_THE_DECISION_LIST;
-import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.RELEASE_STATUS_YES_OR_NO;
-import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.SECRETARY_OF_STATE_YES_OR_NO;
-import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.SS_CONSENT_DECISION;
 import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.SUPPORTER_2_DOB;
 import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.SUPPORTER_3_DOB;
 import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.SUPPORTER_4_DOB;
 import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition.SUPPORTER_DOB;
-import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.field.YesOrNo.NO;
-import static uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.field.YesOrNo.YES;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
@@ -28,25 +19,23 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.bailcaseapi.domain.DateProvider;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCase;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCaseFieldDefinition;
-import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.DecisionType;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.Event;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.callback.Callback;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.callback.DispatchPriority;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
-import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.bailcaseapi.domain.handlers.PreSubmitCallbackHandler;
 
 @Slf4j
 @Component
 public class DateValidationHandler implements PreSubmitCallbackHandler<BailCase> {
 
-    private final static Set<Event> eventsToHandle = Set.of(Event.START_APPLICATION,
+    private static final Set<Event> eventsToHandle = Set.of(Event.START_APPLICATION,
                                                Event.EDIT_BAIL_APPLICATION,
                                                Event.EDIT_BAIL_APPLICATION_AFTER_SUBMIT,
                                                Event.MAKE_NEW_APPLICATION);
 
-    private final static Set<BailCaseFieldDefinition> fieldsToHandle = Set.of(APPLICANT_ARRIVAL_IN_UK,
+    private static final Set<BailCaseFieldDefinition> fieldsToHandle = Set.of(APPLICANT_ARRIVAL_IN_UK,
                                                  BAIL_HEARING_DATE,
                                                  SUPPORTER_DOB,
                                                  SUPPORTER_2_DOB,
@@ -56,7 +45,7 @@ public class DateValidationHandler implements PreSubmitCallbackHandler<BailCase>
     private static final Map<String, BailCaseFieldDefinition> pageIdsToHandle = fieldsToHandle.stream()
         .collect(Collectors.toMap(BailCaseFieldDefinition::value, def -> def));
 
-    private final static String FUTURE_DATE_ERROR_MESSAGE = "The date must not be a future date.";
+    private static final String FUTURE_DATE_ERROR_MESSAGE = "The date must not be a future date.";
 
     private final DateProvider dateProvider;
 
@@ -99,11 +88,16 @@ public class DateValidationHandler implements PreSubmitCallbackHandler<BailCase>
         PreSubmitCallbackResponse<BailCase> response = new PreSubmitCallbackResponse<>(bailCase);
 
         String pageId = callback.getPageId();
-        BailCaseFieldDefinition bailCaseFieldDefinition = pageIdsToHandle.get(pageId);
+        BailCaseFieldDefinition dateField = pageIdsToHandle.get(pageId);
 
-        String date = bailCase.read(bailCaseFieldDefinition, String.class).orElse("");
+        String date = bailCase.read(dateField, String.class).orElse("");
 
-        Optional<LocalDate> optionalDateToBeVerified = parseDate(date, bailCaseFieldDefinition);
+        // if field is optional and left empty, move on without verifications
+        if (date.isBlank() && canBeBlank(dateField)) {
+            return response;
+        }
+
+        Optional<LocalDate> optionalDateToBeVerified = parseDate(date, dateField);
 
         optionalDateToBeVerified.ifPresent(dateToBeVerified -> {
             if (dateToBeVerified.isAfter(now)) {
@@ -114,16 +108,23 @@ public class DateValidationHandler implements PreSubmitCallbackHandler<BailCase>
         return response;
     }
 
-    private boolean isOptional(BailCaseFieldDefinition bailCaseFieldDefinition) {
-        Set<BailCaseFieldDefinition> optionalDefinitions = Set.of(APPLICANT_ARRIVAL_IN_UK);
-        return optionalDefinitions.contains(bailCaseFieldDefinition);
+    // some date fields can be blank either because they're OPTIONAL in CCD or because the field stays hidden based on
+    // a previous answer (e.g. has bail been refused in past 28 days? NO --> BAIL_HEARING_DATE blank)
+    private boolean canBeBlank(BailCaseFieldDefinition dateField) {
+        Set<BailCaseFieldDefinition> optionalDefinitions = Set.of(APPLICANT_ARRIVAL_IN_UK,
+                                                                  BAIL_HEARING_DATE,
+                                                                  SUPPORTER_DOB,
+                                                                  SUPPORTER_2_DOB,
+                                                                  SUPPORTER_3_DOB,
+                                                                  SUPPORTER_4_DOB);
+        return optionalDefinitions.contains(dateField);
     }
 
-    private Optional<LocalDate> parseDate(String date, BailCaseFieldDefinition bailCaseFieldDefinition) {
+    private Optional<LocalDate> parseDate(String date, BailCaseFieldDefinition dateField) {
         try {
             return Optional.of(LocalDate.parse(date));
         } catch (DateTimeParseException e) {
-            log.error("Date [{}] for field [{}] can't be parsed", date, bailCaseFieldDefinition);
+            log.error("Date [{}] for field [{}] can't be parsed", date, dateField);
         }
         return Optional.empty();
     }
