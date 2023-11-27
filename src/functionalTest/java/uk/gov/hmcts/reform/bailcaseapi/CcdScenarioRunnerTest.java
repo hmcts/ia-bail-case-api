461c94c677200b760fc20875d5dd12e172b82368
Ryan Nesbitt <rnesbitt03@qub.ac.uk>
2023-11-27 17:14:19 +0000

    RIA-7996 Change ccdScenarioRunnerTest encoding to UTF8

--- src/functionalTest/java/uk/gov/hmcts/reform/bailcaseapi/CcdScenarioRunnerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bailcaseapi/CcdScenarioRunnerTest.java
@@ -195,7 +195,7 @@ public class CcdScenarioRunnerTest {
                         SerenityRest
                             .given()
                             .headers(authorizationHeaders)
-                            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
                             .body(requestBody)
                             .when()
                             .post(requestUri)
