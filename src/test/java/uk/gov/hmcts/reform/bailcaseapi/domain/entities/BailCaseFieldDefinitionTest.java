c49d0c5408a97bcb63a2d6cfc9d18337e4419ae4
Ivan Kirsanov <ivan.kirsanov@version1.com>
2024-01-29 12:26:40 +0100

    RIA-8232_feature_toggler_ima: Add IMA toggler to Submit application

--- src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/BailCaseFieldDefinitionTest.java
+++ src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/BailCaseFieldDefinitionTest.java
@@ -15,7 +15,7 @@ public class BailCaseFieldDefinitionTest {
      */
     @Test
     void fail_if_changes_needed_after_modifying_bail_case_definition() {
-        assertEquals(227, BailCaseFieldDefinition.values().length);
+        assertEquals(228, BailCaseFieldDefinition.values().length);
     }
 
     @Test
