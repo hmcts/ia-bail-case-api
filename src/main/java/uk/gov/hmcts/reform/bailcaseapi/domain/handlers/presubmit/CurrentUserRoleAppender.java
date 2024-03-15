612b1831c6ba885edaf8520adfc386f750c7c111
nilay <nilay.patel@hmcts.net>
2024-03-15 18:06:09 +0000

    DIAC-232 spike test

--- src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/handlers/presubmit/CurrentUserRoleAppender.java
+++ src/main/java/uk/gov/hmcts/reform/bailcaseapi/domain/handlers/presubmit/CurrentUserRoleAppender.java
@@ -33,7 +33,7 @@ public class CurrentUserRoleAppender implements PreSubmitCallbackHandler<BailCas
         requireNonNull(callback, "callback must not be null");
 
         return callbackStage == PreSubmitCallbackStage.ABOUT_TO_START
-               && (callback.getEvent() == Event.UPLOAD_DOCUMENTS);
+               && (callback.getEvent() == Event.UPLOAD_DOCUMENTS || callback.getEvent() == Event.VIEW_PREVIOUS_APPLICATIONS);
     }
 
     public PreSubmitCallbackResponse<BailCase> handle(
