ec543baf69754675f70f3d8c7508d8126842413e
kleog <kleogeorgiou@hotmail.com>
2024-08-14 10:54:29 +0100

    DIAC-527 - update tests

--- src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/ccd/EventTest.java
@@ -43,6 +43,6 @@ public class EventTest {
 
     @Test
     void fail_if_changes_needed_after_modifying_class() {
-        assertEquals(33, Event.values().length);
+        assertEquals(34, Event.values().length);
     }
 }
