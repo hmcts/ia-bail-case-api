cd9591bb7741dbb17fcdb46c5435a0aa1e541c80
nilay <nilay.patel@solirius.com>
2025-12-10 11:53:04 +0000

    test ffix

--- src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/entities/ccd/EventTest.java
@@ -49,6 +49,6 @@ public class EventTest {
 
     @Test
     void fail_if_changes_needed_after_modifying_class() {
-        assertEquals(39, Event.values().length);
+        assertEquals(40, Event.values().length);
     }
 }
