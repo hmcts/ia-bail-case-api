8a4b60c14e179b2194e48cd23a78d2fc50f8b8ce
nilay <nilay.patel@solirius.com>
2026-05-27 18:11:53 +0100

    fixes

--- src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/utils/HearingCentreUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/bailcaseapi/domain/utils/HearingCentreUtilsTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.BailCase;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.CaseManagementLocation;
 import uk.gov.hmcts.reform.bailcaseapi.domain.entities.CaseManagementLocationRefData;
@@ -21,6 +23,7 @@ import uk.gov.hmcts.reform.bailcaseapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.bailcaseapi.domain.handlers.presubmit.CaseManagementLocationService;
 import uk.gov.hmcts.reform.bailcaseapi.domain.service.LocationRefDataService;
 
+@MockitoSettings(strictness = Strictness.LENIENT)
 @ExtendWith(MockitoExtension.class)
 class HearingCentreUtilsTest {
 
